e6685ca3fdec97549c7e11ac8d50e2f78250691b
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-11-22 11:48:23 -0300

    Accepted @ URI

--- URI-2479.c
+++ URI-2479.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void QuickStringSort(char **, int);
-void swap(const char **, const char **);
+void QuickSortAlgorithm(char string[][20], int, int);
+int HoarePartition(char string[][20], int, int);
+int LomutoPartition(char array[][20], int, int);
 
 int main(void){
-    int coolKids;
+    int coolKids, badKids = 0, index = 0;
     scanf("%d", &coolKids);
 
-    char **string;
-    while((scanf("%s", string[0]) == 1))
+    char tmp, list[coolKids][20];
+    while(index < coolKids){
+        scanf(" %c %s", &tmp, list[index]);
+        if (tmp == '-')
+            badKids++;
+        index++;
+    }
 
-    QuickStringSort(string, sizeof(string) / sizeof(*string));
+    QuickSortAlgorithm(list, 0, coolKids-1);
+    for (int index = 0; index < coolKids; index++)
+		printf("%s\n", list[index]);
 
+    printf("Se comportaram: %d | Nao se comportaram: %d\n", coolKids - badKids, badKids);
     return 0;
 }
 
-void QuickStringSort(char **string, int length){
-    if (length <= 1)
-        return;
-
-    swap(string + ((unsigned int)rand() % length), string + length - 1);
-    
-    int pivot = 0;
-    for (int index = 0; index < length - 1; ++index){
-        if (strcmp(string[index], string[length - 1]) < 0)
-            swap(string + index, string + pivot++);
+void QuickSortAlgorithm(char string[][20], int start, int end){
+    if (start < end){
+        int pi = LomutoPartition(string, start, end); /* pi is partitioning index, string[p] is now at right place */
+        QuickSortAlgorithm(string, start, pi - 1);   /* Separately sort elements before partition and after partition */
+        QuickSortAlgorithm(string, pi + 1, end);
     }
-
-    swap(string + pivot, string + length - 1);
-    QuickStringSort(string, pivot++);
-    QuickStringSort(string + pivot, length - pivot);
 }
 
-/*** swap Function: uses call-by-reference to swap values ***/
-void swap(const char **pastString, const char **thisString){
-    const char *temp = *pastString;   /* Saving `x` value in `temp`                        */
-    *pastString = *thisString;        /* Setting `x` value to `y` value                    */
-    *thisString = temp;               /* Setting `iterato` value to `temp` value (old `x`) */
+int LomutoPartition(char array[][20], int start, int end){ 
+    char pivot[20];    
+    strcpy(pivot, array[end]);
+    int index = (start - 1);                     /* Index of smaller element */
+  
+    for (int element = start; element <= end-1; element++){ 
+        if (strcmp(array[element], pivot) < 0){  /* If current element is smaller than or equal to pivot */ 
+            index++;      
+            char aux[20];                        /* increment index of smaller element */
+            strcpy(aux, array[index]); 
+            strcpy(array[index], array[element]);
+            strcpy(array[element], aux);
+        } 
+    }
+    char aux[20];
+    strcpy(aux, array[index + 1]);
+    strcpy(array[index + 1], array[end]);
+    strcpy(array[end], aux);
+
+    return (index + 1); 
 }
